60e4b189a9186363d6915cb03b5fbd998c349fd5
fredy <fredyalejandroleyva@gmail.com>
2023-06-05 17:27:53 -0500

    Trabajo a medias

---
+++ clases/clasesSalario.py
@@ -0,0 +1,7 @@
+class Salario:
+        def __init__(self, nombre, cargo, salario):
+            self.__nombre=nombre
+            self.__cargo=cargo
+            self.__salario=salario
+        def nombre(self):
+        
